8bae19a22e705dc1de51c26996070a36fe8395df
Vit <415816@bk.ru>
2021-06-27 23:02:20 +0300

    creat new files

---
+++ slider.js
@@ -0,0 +1,28 @@
+let slider1 = {
+    imgesURLs: [],
+    currentImageIndex: 0,
+    showPrewBtn: document.querySelector('.show-prew-btn'),
+    showNextBtn: document.querySelector('.show-next-btn'),
+    slideImage: document.querySelector('.slide-img'),
+    start: function(){
+        this.showPrewBtn.onclick = this.showPrewImg;
+        this.showNextBtn.onclick = this.showNextImg;
+        this.imgesURLs.push('https://cdnimg.rg.ru/img/content/178/22/40/kotik_d_850.jpg');
+        this.imgesURLs.push('https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png');
+        this.imgesURLs.push('https://ichef.bbci.co.uk/news/640/cpsprodpb/14A82/production/_116301648_gettyimages-1071204136.jpg');
+        this.imgesURLs.push('https://static.ngs.ru/news/2020/99/preview/8290b39b6601efa615afd4bbd93d82320a039883_720_405_c.png');
+        this.slideImage.src = this.imgesURLs[this.currentImageIndex];
+        this.showPrewBtn.disabled = true;
+    },
+    showPrewImg: function(event){
+        this.currentImageIndex--;
+        this.slideImage.src = this.imgesURLs[this.currentImageIndex];
+        this.showPrewBtn.disabled = true;
+    },
+    showNextImg: function(event){
+        this.currentImageIndex++;
+        this.slideImage.src = this.imgesURLs[this.currentImageIndex];
+        if(this.currentImageIndex == (this.imgesURLs.length - 1)) {this.showNextBtn.disabled = true;}
+    }
+
+}
