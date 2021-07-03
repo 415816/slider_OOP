1e20f80a7112dfb3e19ae972a61f78edbccdd371
Vit <415816@bk.ru>
2021-07-03 21:30:00 +0300

    add 'construct new'

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+/* let slider1 = sliderFactory.creatNewSlider();
+let slider2 = sliderFactory.creatNewSlider(); */
+let slider1 = new Slider;
+let slider2 = new Slider;
+
+
 slider1.start('slider1');
 slider2.start('slider2');
